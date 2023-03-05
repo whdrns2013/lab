06edecb9e0da4b0bc15cde44aea49b0e905cf510
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2023-03-05 20:24:12 +0900

    Delete transport_to_aws.py

--- 20230210_sesac_final/transport_to_aws.py
+++
@@ -1,6 +0,0 @@
-import pymysql as m
-
-con = m.connect(host = '54.164.15.159', db = 'team06-antifragile-db',
-                user = 'admin', password = 'antifragile1234', charset = 'utf8')
-
-cur = con.cursor()
