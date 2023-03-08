41d9ab773ab8086c962c0ddc5137bafa24a8002f
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2023-03-08 20:40:22 +0900

    updt project

---
+++ 20230210_sesac_final/antifragile1.sql
@@ -0,0 +1,164 @@
+-- MySQL dump 10.14  Distrib 5.5.68-MariaDB, for Linux (x86_64)
+--
+-- Host: team06-antifragile-db.cxuncqkdvk3h.us-east-1.rds.amazonaws.com    Database: antifragile
+-- ------------------------------------------------------
+-- Server version	10.6.11-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cam`
+--
+
+DROP TABLE IF EXISTS `cam`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cam` (
+  `cam_id` varchar(20) NOT NULL,
+  `user_uid` varchar(20) NOT NULL,
+  `cam_name` varchar(40) NOT NULL,
+  `loc_lat` double DEFAULT NULL,
+  `loc_long` double DEFAULT NULL,
+  PRIMARY KEY (`cam_id`),
+  KEY `user_id` (`user_uid`),
+  CONSTRAINT `cam_ibfk_1` FOREIGN KEY (`user_uid`) REFERENCES `user` (`user_uid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cam`
+--
+
+LOCK TABLES `cam` WRITE;
+/*!40000 ALTER TABLE `cam` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cam` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `image_id` varchar(40) NOT NULL,
+  `image_path` varchar(50) NOT NULL,
+  `exp` varchar(10) NOT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `log_id` varchar(40) NOT NULL,
+  `user_uid` varchar(20) NOT NULL,
+  `cam_id` varchar(20) NOT NULL,
+  `log_type` varchar(10) NOT NULL,
+  `time_stamp` datetime NOT NULL,
+  `object_id` varchar(20) NOT NULL,
+  `boundingbox_01` varchar(100) DEFAULT NULL,
+  `boundingbox_02` varchar(100) DEFAULT NULL,
+  `mid_location` varchar(100) DEFAULT NULL,
+  `image_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `user_uid` (`user_uid`),
+  KEY `cam_id` (`cam_id`),
+  KEY `object_id` (`object_id`),
+  CONSTRAINT `log_ibfk_1` FOREIGN KEY (`object_id`) REFERENCES `object` (`object_id`) ON UPDATE CASCADE,
+  CONSTRAINT `log_ibfk_2` FOREIGN KEY (`cam_id`) REFERENCES `cam` (`cam_id`) ON UPDATE CASCADE,
+  CONSTRAINT `log_ibfk_3` FOREIGN KEY (`user_uid`) REFERENCES `cam` (`user_uid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log`
+--
+
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `object`
+--
+
+DROP TABLE IF EXISTS `object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `object` (
+  `object_id` varchar(20) NOT NULL,
+  `object_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `object`
+--
+
+LOCK TABLES `object` WRITE;
+/*!40000 ALTER TABLE `object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_uid` varchar(20) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `user_pw` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-04 18:04:49
