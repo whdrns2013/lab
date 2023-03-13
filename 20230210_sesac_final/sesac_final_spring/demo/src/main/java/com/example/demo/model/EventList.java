2856b6e06207965531dc95a8efb54cf4b5e23bb2
whdrns2013 <whdrns2013@naver.com>
2023-03-13 19:18:47 +0900

    updt project

---
+++ 20230210_sesac_final/sesac_final_spring/demo/src/main/java/com/example/demo/model/EventList.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class EventList {
+    
+    String event_name;
+    String detecte_date;
+    String detect_time;
+    String object1_name;
+    String object1_num;
+    String object2_name;
+    String object2_num;
+    String object3_name;
+    String object3_num;
+
+}
