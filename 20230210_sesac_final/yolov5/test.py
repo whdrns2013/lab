cfdbae41c7809d285d7fb614e1a115e17a97eb13
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2023-03-13 12:37:34 +0900

    Update test.py

--- 20230210_sesac_final/yolov5/test.py
+++ 20230210_sesac_final/yolov5/test.py
@@ -1,5 +1,17 @@
-import pygame
+import os
 
-pygame.init()
-pygame.mixer.music.load('./beep.wav')
-pygame.mixer.music.play()
+path = '/Users/jongya/Desktop/Workspace/lab/20230210_sesac_final/yolov5'
+# os.chdir(path)
+
+# import pygame
+# pygame.init()
+# sound = pygame.mixer.Sound(path + '/beep.wav')
+# sound.play()
+# pygame.time.wait(int(sound.get_length() * 1000))
+# pygame.quit()
+
+from playsound import playsound
+playsound(path + '/beep.wav')
+
+# import winsound
+# winsound.PlaySound(path + '/beep.wav', winsound.SND_FILENAME)
