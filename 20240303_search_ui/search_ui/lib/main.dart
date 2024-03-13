9a219ddd7b6df009649a8cb9e18ee4a40a8987e8
Jonghyuk Park <whdrns2013@naver.com>
2024-03-13 22:03:42 +0900

    Update main.dart

--- 20240303_search_ui/search_ui/lib/main.dart
+++ 20240303_search_ui/search_ui/lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SearchResult(),
+      home: HomeScreen(),
     );
   }
 }
