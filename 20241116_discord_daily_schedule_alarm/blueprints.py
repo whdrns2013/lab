ac1e9cc6aad879a06b575c318e99c83f7a37399b
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2024-11-16 20:12:00 +0900

    update project

---
+++ 20241116_discord_daily_schedule_alarm/blueprints.py
@@ -0,0 +1,33 @@
+
+
+
+class DateReqBody:
+    def __init__(self, property_name:str, today:str):
+        self.body = {
+            'filter': {
+                'and': [
+                    {
+                    'property': property_name,
+                    'date': {
+                        'on_or_before': today,
+                        'start':True
+                    }
+                    },
+                    {
+                    'property': property_name,
+                    'date': {
+                        'on_or_after': today,
+                        'end':True
+                    }
+                    }
+                ],
+                'or':[
+                    {
+                        'property': property_name,
+                        'date': {
+                            'equal': today                            
+                        }
+                    }
+                ]
+            }
+        }
