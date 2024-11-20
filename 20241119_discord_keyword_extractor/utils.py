f34677c26bbb104b64e5e4c407a105c4889635d4
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2024-11-20 22:51:26 +0900

    update

--- 20241119_discord_keyword_extractor/utils.py
+++ 20241119_discord_keyword_extractor/utils.py
@@ -1,4 +1,9 @@
+from typing import List
 
-def save_nouns(file_path, nouns):
-    with open(file_path) as f:
-        f.write(nouns)
+
+def save_nouns(file_path:str, nouns:List[str]):
+    text = ''
+    for noun in nouns:
+        text = text + ' ' + noun
+    with open(file_path, 'w') as f:
+        f.write(text)
